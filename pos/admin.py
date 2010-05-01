b038fe7dbd09170164fe1a6320ec32b26e196f0a
hemanth <leohemanth@gmail.com>
2010-05-01 18:52:19 +0530

    models

--- pos/admin.py
+++ pos/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
 from pos.models import *
 
+class PurchaseInvoiceItemadmin(admin.ModelAdmin):
+    list_display = ('invoice', 'product','quantity','price','cost_price')
+    search_fields = ('invoice', 'product','quantity','price','cost_price')
+
+class SaleInvoiceadmin(admin.ModelAdmin):
+    list_display = ('customer','datetime')
+    search_fields = ('customer','datetime')
+	
+class ItemsInline(admin.TabularInline):
+	model = PurchaseInvoiceItem
+
+class PurchaseInvoiceadmin(admin.ModelAdmin):
+	inlines = [ItemsInline,]
 
 
-admin.site.register()
-
+admin.site.register(PurchaseInvoiceItem,PurchaseInvoiceItemadmin)
+admin.site.register(PurchaseInvoice,PurchaseInvoiceadmin)
+admin.site.register(PurchaseInvoice)
+admin.site.register(SaleInvoice,SaleInvoiceadmin)
+admin.site.register(Product)
+admin.site.register(ProductPrice)
+admin.site.register(Payment)
+admin.site.register(CompanyContact)
+admin.site.register(Company)
+admin.site.register(Customer)
+admin.site.register(Category)
